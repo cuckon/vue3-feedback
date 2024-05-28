732ec62d0f4a008e6842bc20213ec91b03450433
John <cuckons@gmail.com>
2024-05-28 14:40:17 +0800

    basic query

--- backend/README.md
+++ backend/README.md
@@ -6,6 +6,11 @@ uvicorn src.main:app --reload
 ```
 
 or if you are put env under `env/`:
+posix:
 ```sh
 env/bin/python -m uvicorn src.main:app --reload
+```
+windows:
+```ps
+.\env\Scripts\python.exe -m uvicorn src.main:app --reload
 ```
--- backend/src/main.py
+++ backend/src/main.py
@@ -27,6 +27,7 @@ def get_db():
     )['feedback']
     return DB_CLIENT
 
+
 class PerFeedback(BaseModel):
     to: str
     rate: int
@@ -38,6 +39,87 @@ class Feedback(BaseModel):
     user: str
     timestamp: int
     feedbacks: List[PerFeedback]
+
+
+async def get_data_from_db():
+    db = get_db()
+    collection = db['feedbacks']
+    pipeline = [
+        {"$group": {
+            "_id": "$to",
+            "data": {
+                "$push": {
+                    "user": "$user",
+                    "timestamp": "$timestamp",
+                    "rate": "$rate",
+                    "good_comment":
+                    "$good_comment",
+                    "bad_comment":
+                    "$bad_comment"
+                }
+            }
+        }},
+        {"$project": {"_id": 0, "to": "$_id", "data": 1}},
+        {"$sort": {"user": 1}},
+    ]
+    result = [doc async for doc in collection.aggregate(pipeline)]
+    return result
+
+def write_excel(data):
+    import xlwt
+    from datetime import datetime
+
+    style_title = xlwt.easyxf('font:bold on')
+    pattern = xlwt.Pattern()
+    pattern.pattern = xlwt.Pattern.SOLID_PATTERN
+    pattern.pattern_fore_colour = xlwt.Style.colour_map['gray25']
+    style_date.pattern = pattern
+
+    style_date = xlwt.easyxf(num_format_str='D-MMM-YY')
+    wb = xlwt.Workbook()
+    ws = wb.add_sheet('Feedbacks')
+
+    current_ln = 0
+
+    # write header
+    ws.write(0, 0, 'Feedbacker', style_title)
+    ws.write(0, 1, 'date', style_title)
+    ws.write(0, 2, 'to', style_title)
+    ws.write(0, 3, 'rate', style_title)
+    ws.write(0, 4, 'good_comment', style_title)
+    ws.write(0, 5, 'bad_comment', style_title)
+
+    for per_feedback_engineer in data:
+        ws.write(current_ln, 0, per_feedback_engineer['user'])
+        # TODO: full header
+
+        ws.write(
+            current_ln, 1,
+            datetime.fromtimestamp(per_feedback_engineer['data']['timestamp']),
+            style_date
+        )
+        ws.write(current_ln, 2, per_feedback_engineer['data']['user'])
+        ws.write(current_ln, 3, per_feedback_engineer['data']['rate'])
+        ws.write(current_ln, 3, per_feedback_engineer['data']['good_comment'])
+        ws.write(current_ln, 3, per_feedback_engineer['data']['bad_comment'])
+        current_ln += 1
+
+
+
+@app.get('/download_summary')
+async def download_summary():
+
+    return await get_data_from_db()
+
+    import xlwt
+    from datetime import datetime
+
+
+    ws.write(0, 0, 'Feedbacker')
+    ws.write(1, 0, datetime.now(), style_date)
+    ws.write(2, 0, 'Feedbacker')
+
+
 
 
 @app.get('/engineers')
